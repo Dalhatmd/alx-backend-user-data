b2f8d96a06d088bdad609c4104cdd604549d975a
dalhatmd <umarwalid4055@gmail.com>
2024-11-11 10:58:31 +0100

    Add filter method

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -12,7 +12,14 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
+auth = os.getenv('AUTH_TYPE')
+if auth:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+excluded_list = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -31,6 +38,21 @@ def forbidden(error) -> str:
     """forbidden error handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+@app.before_request
+def filter():
+    """Filters requests
+    """
+    if not auth.require_auth(request.path, excluded_list):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
+
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
