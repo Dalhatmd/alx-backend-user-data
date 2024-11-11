c558b856011764888bf00e418e2a9e287a4d46e0
dalhatmd <umarwalid4055@gmail.com>
2024-11-11 10:01:54 +0100

    update require_auth method

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -8,8 +8,18 @@ class Auth:
     """ Authentication functionailty
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """doc """
-        return False
+        """ Checks if a path requires authorization
+        """
+        if path is None or excluded_paths is None:
+            return True
+
+        path = path if path.endswith('/') else path + '/'
+
+        for excluded_path in excluded_paths:
+            if path == excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ doc """
