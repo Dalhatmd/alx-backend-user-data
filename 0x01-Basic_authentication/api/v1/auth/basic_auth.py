e125eb718613f366e9bf13d3ce7c0edaf3171e05
dalhatmd <umarwalid4055@gmail.com>
2024-11-12 22:19:48 +0100

    Complete auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -71,3 +71,15 @@ class BasicAuth(Auth):
             return None
 
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ retrieves the User instance for a request
+        """
+        auth_header = self.authorization_header(request)
+        auth = self.extract_base64_authorization_header(auth_header)
+        decoded = self.decode_base64_authorization_header(auth)
+        (username, password) = self.extract_user_credentials(decoded)
+        user = self.user_object_from_credentials(username, password)
+
+        return user
+
