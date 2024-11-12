a4ea95f0a83b26d7e8722279eae4e64752e074ec
dalhatmd <umarwalid4055@gmail.com>
2024-11-12 17:58:31 +0100

    Fix auth selection

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,6 +7,8 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -15,8 +17,9 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 auth = os.getenv('AUTH_TYPE')
-if auth:
-    from api.v1.auth.basic_auth import BasicAuth
+if auth == 'auth':
+    auth = Auth()
+elif auth == 'basic_auth':
     auth = BasicAuth()
 
 excluded_list = ['/api/v1/status/',
