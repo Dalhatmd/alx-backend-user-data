4f7a9c51d6b77f582fba628543fc13399cbfe8e1
dalhatmd <umarwalid4055@gmail.com>
2024-11-18 11:01:50 +0100

    Add find_user method

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,52 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from user import User
+from user import Base
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+
+    def find_user_by(self, **kwargs: str) -> User:
+        """searches for a user in db
+        """
+        valid_args = ('email',)
+        for key in kwargs.keys():
+            if key == "email":
+                user = self._session.query(User).filter_by(email=kwargs[key]).first()
+                if not user:
+                    raise NoResultFound
+                else:
+                    return user
+            if key not in valid_args:
+                raise InvalidRequestError
