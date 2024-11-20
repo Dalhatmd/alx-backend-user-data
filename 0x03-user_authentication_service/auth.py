8c87709bd750b0919203a26f8ef9b9d7493c1d13
dalhatmd <umarwalid4055@gmail.com>
2024-11-20 18:45:38 +0100

    Add method to find user by session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -58,3 +58,12 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+       def get_user_from_session_id(self, session_id):
+           """ finds user corresponding to session_id
+           """
+           try:
+               user = self._db.find_user_by(session_id)
+               return user
+           except NoResultFound:
+               return None
