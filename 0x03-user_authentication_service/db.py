a772b0593fae7c1116ee416cc704d3022777acc5
dalhatmd <umarwalid4055@gmail.com>
2024-11-22 17:16:31 +0100

    add docs to add_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -36,6 +36,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
+        """ adds a news user to database """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
